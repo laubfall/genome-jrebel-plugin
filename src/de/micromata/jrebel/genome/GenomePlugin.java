1d12799cf0b7a785bdc493802d76b286a2e83c6f
Neeme Praks <neeme.praks@zeroturnaround.com>
2014-12-15 16:41:47 +0200

    Skeleton of JRebel plugin

---
+++ src/de/micromata/jrebel/genome/GenomePlugin.java
@@ -0,0 +1,51 @@
+package de.micromata.jrebel.genome;
+
+import org.zeroturnaround.javarebel.ClassResourceSource;
+import org.zeroturnaround.javarebel.Integration;
+import org.zeroturnaround.javarebel.IntegrationFactory;
+import org.zeroturnaround.javarebel.Plugin;
+
+public class GenomePlugin implements Plugin {
+
+  public void preinit() {
+    ClassLoader cl = getClass().getClassLoader();
+    Integration integration = IntegrationFactory.getInstance();
+
+//    integration.addIntegrationProcessor(cl,
+//        "de.micromata.genome.web.gwar.bootstrap.StorageClassLoader",
+//        new StorageClassLoaderCBP());
+  }
+
+  public boolean checkDependencies(ClassLoader cl, ClassResourceSource crs) {
+    return crs.getClassResource("de.micromata.genome.web.gwar.bootstrap.StorageClassLoader") != null;
+  }
+
+  public String getDescription() {
+    return "<li>Enables reloading of Java classes in GWAR apps</li>";
+  }
+
+  public String getId() {
+    return "genome_plugin";
+  }
+
+  public String getName() {
+    return "Genome plugin";
+  }
+
+  public String getWebsite() {
+    return "http://www.micromata.de/";
+  }
+
+  public String getAuthor() {
+    return null;
+  }
+
+  public String getSupportedVersions() {
+    return null;
+  }
+
+  public String getTestedVersions() {
+    return null;
+  }
+
+}
